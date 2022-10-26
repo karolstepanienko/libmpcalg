2f9bf0262f8ed65185d8e77878a18423a2254967
karolstepanienko <karol.stepanienko@gmail.com>
2022-10-26 21:18:01 +0200

    test: Parallel test execution.

---
+++ test/runAllTestsOctaveParallel.m
@@ -0,0 +1,25 @@
+function runAllTestsOctaveParallel
+    choosenTests = {'testDMC', 'testDMCParameters', 'testGPC', 'testMPCS',...
+        'testMPCSParameters'};
+    vector_y = pararrayfun(nproc - nproc*0.1, @runTestsOctaveParallel,...
+        choosenTests);
+end
+
+function result = runTestsOctaveParallel(choosenTest)
+    if strcmp(choosenTest, 'testDMC')
+        test testDMC
+        result = 1;
+    elseif strcmp(choosenTest, 'testDMCParameters')
+        test testDMCParameters
+        result = 1;
+    elseif strcmp(choosenTest, 'testGPC')
+        test testGPC
+        result = 1;
+    elseif strcmp(choosenTest, 'testMPCS')
+        test testMPCS
+        result = 1;
+    elseif strcmp(choosenTest, 'testMPCSParameters')
+        test testMPCSParameters
+        result = 1;
+    end
+end
