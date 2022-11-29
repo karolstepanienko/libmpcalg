fd5c3f1edd2ffb909cd15b43ca65149528f72b7e
karolstepanienko <karol.stepanienko@gmail.com>
2022-11-29 05:32:30 +0100

    test: Absolute error test adjustments

--- test/absoluteControlError/testDMC.m
+++ test/absoluteControlError/testDMC.m
@@ -2,7 +2,7 @@
 
 %!assert(runAlg('1x1', @DMC, 'analytical', false) < 20)
 %!assert(runAlg('1x1', @DMC, 'fast', false) < 20)
-%!assert(runAlg('1x1', @DMC, 'numerical', false) < 40)
+%!assert(runAlg('1x1', @DMC, 'numerical', false) < 20)
 
 %!assert(runAlg('1x2', @DMC, 'analytical', false) < 20)
 %!assert(runAlg('1x2', @DMC, 'fast', false) < 20)
--- test/absoluteControlError/testGPC.m
+++ test/absoluteControlError/testGPC.m
@@ -2,7 +2,7 @@
 
 %!assert(runAlg('1x1', @GPC, 'analytical', false) < 20)
 %!assert(runAlg('1x1', @GPC, 'fast', false) < 20)
-%!assert(runAlg('1x1', @GPC, 'numerical', false) < 40)
+%!assert(runAlg('1x1', @GPC, 'numerical', false) < 20)
 
 %!assert(runAlg('1x2', @GPC, 'analytical', false) < 20)
 %!assert(runAlg('1x2', @GPC, 'fast', false) < 20)
